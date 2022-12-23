0510092844f203ab514d4e7a11e68be787f803b5
JackHMcD <JackHMcD@gmail.com>
2022-12-22 21:28:43 -0700

    Update package_lists.sh

--- scripts/Shared/package_lists.sh
+++ scripts/Shared/package_lists.sh
@@ -59,12 +59,10 @@ base_debs_install=(
     net-tools
     netcat-openbsd
     network-manager
-    openssh-server
     parted
     pciutils
     psmisc
     rsync
-    sudo
     traceroute
     udev
     usbutils
